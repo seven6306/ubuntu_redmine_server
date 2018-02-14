f76662d4f6c85cb3a3cc3d51078363bd9064e90f
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-14 13:11:49 +0800

    Update file_backup.py

--- lib/file_backup.py
+++ lib/file_backup.py
@@ -1,1 +1,26 @@
+#!/usr/bin/python
+from sys import argv
+from shtuil import copy
+from os import remove, makedirs
+from os.path import join, isfile, isdir, basename
 
+def backup(files):
+    try:
+        if not isdir('backup'):
+            makedirs('backup')
+        for each_file in files.split(','):
+            bak = join('backup', basename(each_file))
+            if isfile(bak):
+                remove(bak)
+            copy(each_file, bak)
+        return True
+    except Exception as err:
+        print str(err)
+    
+if __name__ == '__main__':
+    if len(argv) == 2:
+        files = argv[1]
+        if not backup(files):
+            raise SystemExit(2)
+    else:
+        'Usage: python file_backup.py [Files]\ne.g., python file_backup.py /home/blake/bak1.conf,/home/blake/bak2.txt'
